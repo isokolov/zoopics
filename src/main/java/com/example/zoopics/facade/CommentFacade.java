c23b294d3eb202a8add898fa42f0fa2853673880
Illya Sokolov <illya.sokolov82@gmail.com>
2021-03-21 16:20:26 +0200

    Facades and Controllers

---
+++ src/main/java/com/example/zoopics/facade/CommentFacade.java
@@ -0,0 +1,19 @@
+package com.example.zoopics.facade;
+
+import com.example.zoopics.dto.CommentDTO;
+import com.example.zoopics.entity.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentFacade {
+
+    public CommentDTO commentToCommentDTO(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setUsername(comment.getUsername());
+        commentDTO.setMessage(comment.getMessage());
+
+        return commentDTO;
+    }
+
+}
---
+++ src/main/java/com/example/zoopics/facade/UserFacade.java
@@ -0,0 +1,21 @@
+package com.example.zoopics.facade;
+
+import com.example.zoopics.dto.UserDTO;
+import com.example.zoopics.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFacade {
+
+    public UserDTO userToUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstname(user.getName());
+        userDTO.setLastname(user.getLastname());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setBio(user.getBio());
+        return userDTO;
+    }
+
+}
+
